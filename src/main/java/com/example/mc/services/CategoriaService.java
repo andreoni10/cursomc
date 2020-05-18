3d63851e5161dc855a4b3197f3179dd49fbd6f50
Lucas Andreoni <lucasandreoni2000@gmail.com>
2020-05-18 14:39:18 -0300

    Ajustes no endpoint /categotias/{id}

--- src/main/java/com/example/mc/services/CategoriaService.java
+++ src/main/java/com/example/mc/services/CategoriaService.java
@@ -2,12 +2,12 @@ package com.example.mc.services;
 
 import java.util.Optional;
 
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.mc.domain.Categoria;
 import com.example.mc.repositories.CategoriaRepository;
+import com.example.mc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
