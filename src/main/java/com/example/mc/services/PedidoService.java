a1a633138d476ef1bef169e48ef95a0b3dce791b
Lucas Andreoni <lucasandreoni2000@gmail.com>
2020-05-20 14:29:50 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/example/mc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.example.mc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.mc.domain.Pedido;
+import com.example.mc.repositories.PedidoRepository;
+import com.example.mc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo; //Repository
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() ->  new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName(), null));
+	}
+
+}
