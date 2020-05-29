fdd4ac2cd9d8c6ad947ee212d09b2007138cc216
Lucas Andreoni <lucasandreoni2000@gmail.com>
2020-05-29 17:20:04 -0300

    Implementando autenticação e geração do token JWT

--- src/main/java/com/example/mc/services/DBService.java
+++ src/main/java/com/example/mc/services/DBService.java
@@ -114,7 +114,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		Cliente cli1 = new Cliente(null, "Maria Silva", "m@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
 		Cliente cli2 = new Cliente(null, "Ana Costa", "@gmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
