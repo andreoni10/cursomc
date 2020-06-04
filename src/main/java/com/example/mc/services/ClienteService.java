22dacee2336bcab37309ce64b4131660d4834c38
Lucas Andreoni <lucasandreoni2000@gmail.com>
2020-06-04 15:23:29 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/example/mc/services/ClienteService.java
+++ src/main/java/com/example/mc/services/ClienteService.java
@@ -1,10 +1,12 @@
 package com.example.mc.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -22,6 +24,8 @@ import com.example.mc.dto.ClienteDTO;
 import com.example.mc.dto.ClienteNewDTO;
 import com.example.mc.repositories.ClienteRepository;
 import com.example.mc.repositories.EnderecoRepository;
+import com.example.mc.security.UserSS;
+import com.example.mc.services.exceptions.AuthorizationException;
 import com.example.mc.services.exceptions.DataIntegrityException;
 import com.example.mc.services.exceptions.ObjectNotFoundException;
 
@@ -39,6 +43,12 @@ public class ClienteService {
 	
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -104,7 +114,15 @@ public class ClienteService {
 	}
 	
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		return s3Service.uploadFile(multipartFile);
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
+		
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
 
 }
