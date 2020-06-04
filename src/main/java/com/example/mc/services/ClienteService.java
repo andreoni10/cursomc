6d4d17f7b3f255cfb577f296ea0fbf299dd5ece4
Lucas Andreoni <lucasandreoni2000@gmail.com>
2020-06-04 16:22:33 -0300

    Bônus: ajustando tamanho da imagem

--- src/main/java/com/example/mc/services/ClienteService.java
+++ src/main/java/com/example/mc/services/ClienteService.java
@@ -50,6 +50,9 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
+	@Value("${img.profile.size}")
+	private Integer size;
+	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() ->  new ObjectNotFoundException(
@@ -120,6 +123,9 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
